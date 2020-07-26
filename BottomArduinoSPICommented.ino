29c847196a3efc8f1715818cc82d27962385d634
yanruyu <32486980+yanruyu126@users.noreply.github.com>
2020-07-26 10:05:26 -0400

    Update BottomArduinoSPICommented.ino

--- BottomArduinoSPICommented.ino
+++ BottomArduinoSPICommented.ino
@@ -158,28 +158,26 @@ ISR (SPI_STC_vect) { //SPI Interrupt Service Routine
   
 }  
 
-//Adjust PWM FOR PID CODE
+/** Adjust PWM for PID algorithm */
 void adjustPWM() {
-  int speedNow0 = calculateSpeed0();
-  int error0 = setpoint - speedNow0;
+  int speedNow0 = calculateSpeed0(); // calculate the current speed for the right motor
+  int error0 = setpoint - speedNow0; // calculate the error between the current speed and the set speed
   double dError0 = ((double)speedNow0 - (double)lastSpeed0) / timeSec;
-  Integral0 += (double) error0;
+  Integral0 += (double) error0; // update integral of the error
 
-  int speedNow1 = calculateSpeed1();
+  int speedNow1 = calculateSpeed1(); // calculate the current speed for the left motor
   int error1 = setpoint - speedNow1;
   double dError1 = ((double)speedNow1 - (double)lastSpeed1) / timeSec;
   Integral1 += (double) error1;
 
+  // cap the integral value within 0..255
   if (Integral0 > 255) Integral0 = 255;
   else if (Integral0 < 0) Integral0 = 0;
-//  Serial.print("Integral0: ");
-//  Serial.println( Integral0);
 
   if (Integral1 > 255) Integral1 = 255;
   else if (Integral1 < 0) Integral1 = 0;
-//  Serial.print("Integral1: ");
-//  Serial.println( Integral1);
 
+  // calculate the value for speed adjustments
   int adjust0 = (kP * (double)error0) + kI * Integral0 + kD * dError0;
   int adjust1 = (kP * (double)error1) + kI * Integral1 + kD * dError1;
 
@@ -189,73 +187,51 @@ void adjustPWM() {
   if (digital1 == 0) pwm1 += adjust1;    
   else pwm1 -= adjust1;
 
+  // cap the pwm values within 0..255
   if (pwm0 > 255) pwm0 = 255;
   else if (pwm0 < 0) pwm0 = 0;
   
   if (pwm1 > 255) pwm1 = 255;
   else if (pwm1 < 0) pwm1 = 0;
 
+  // store the current speeds
   lastSpeed0 = speedNow0;
   lastSpeed1 = speedNow1;
-//
-//  Serial.print("adjustment0: ");
-//  Serial.println( adjust0);
-//  Serial.print("PWM0: ");
-//  Serial.println( pwm0 );
-//
-//  Serial.print("adjustment1: ");
-//  Serial.println( adjust1);
-//  Serial.print("PWM1: ");
-//  Serial.println( pwm1 );
 }
 
-
+/** Return the current rotational speed of right motor with encoder data. */
 int calculateSpeed0() {
   int speedDetect = (encoder0Pos - encoder0PrevCount) / timeSec;
-//  Serial.print("Encoder0pos: ");
-//  Serial.print( encoder0Pos );
-//  Serial.print("  ");
-//  Serial.println( encoder0PrevCount);
   encoder0PrevCount = encoder0Pos;
-//  Serial.print( "Speed0: ");
-//  Serial.println( speedDetect);
   return speedDetect;
 }
 
+
+/** Return the current rotational speed of left motor with encoder data. */
 int calculateSpeed1() {
   int speedDetect = (encoder1Pos - encoder1PrevCount) / timeSec;
-//  Serial.print("Encoder0pos: ");
-//  Serial.print( encoder1Pos);
-//  Serial.print("  ");
-//  Serial.println( encoder1PrevCount);
   encoder1PrevCount = encoder1Pos;
-//  Serial.print( "Speed1: ");
-//  Serial.println( speedDetect);
   return speedDetect;
 }
 
+
+/** Adjust the speed of motors with the PID algorithm. */
 void PID() {
-   if (digital0 == 1)
-    digitalWrite( motor0pin1, HIGH);
+  if (digital0 == 1) digitalWrite( motor0pin1, HIGH);
   else digitalWrite( motor0pin1, LOW);
   analogWrite( motor0pin2, pwm0);
 
-  if (digital1 == 1)
-    digitalWrite( motor1pin1, HIGH);
+  if (digital1 == 1) digitalWrite( motor1pin1, HIGH);
   else digitalWrite( motor1pin1, LOW);
   analogWrite( motor1pin2, pwm1);
 
-
   timeElapsed = 0;
-  //calculateSpeed();
   while ( timeElapsed < 500 ) {
 
     n = digitalRead(encoder0PinA);
     if ((encoder0PinALast == LOW) && (n == HIGH)) {
       encoder0Pos++;
     }
-    //Serial.print (encoder0Pos);
-    //Serial.print ("/");
 
     encoder0PinALast = n;
 
@@ -263,8 +239,6 @@ void PID() {
     if ((encoder1PinALast == LOW) && (m == HIGH)) {
       encoder1Pos++;
     }
-    // Serial.print (encoder1Pos);
-    //Serial.print ("/");
 
     encoder1PinALast = m;
 
@@ -273,10 +247,7 @@ void PID() {
   //unsigned long CurrentTime = millis();
   //unsigned long ElapsedTime = CurrentTime - StartTime;
   timeSec = 1.0 ;//(double)( ElapsedTime * .001);
-//  Serial.print( "Time: ");
-//  Serial.println(timeSec); // time needs to be fixed
   adjustPWM();
-//  Serial.println(" ");
 
 }
 
